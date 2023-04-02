2c3c36b1f81047601b34e51bc0804fbacbb8c2a0
Max Duchesne <max.duchesne@gmail.com>
2023-04-02 12:33:59 -0400

    update settings

--- syllabus_finder/settings.py
+++ syllabus_finder/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d6m2x(psb-f%9(k82s9=uvrkbonvfaje2sbi0ij24z2q^^@g8k'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
